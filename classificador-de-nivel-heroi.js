f95241a7213a282f945235a0a1a1b36db1985ba4
Jackeline Silva <jackelinesilva.tech@gmail.com>
2024-07-13 22:10:02 -0300

    Estudos - Primeiro Desafio de Lógica de Programação
    No primeiro desafio do Curso Lógica de Programação da DIO (tendo como base a linguagem de programação Javascript), fomos convidados a realizar um classificador de nível de herói usando o que foi visto até aquele momento (variáveis, operadores, laços de repetição & estruturas de decisões).

---
+++ classificador-de-nivel-heroi.js
@@ -0,0 +1,30 @@
+
+let experienceScore= 10001
+
+if (experienceScore<= 1000) {
+    console.log ("= ferro")
+}
+
+else if (experienceScore<= 2000) {
+    console.log ("= bronze")
+}
+
+else if (experienceScore<= 5000) {
+    console.log ("= prata ouro")
+}
+
+else if (experienceScore<= 8000) {
+    console.log ("= platina diamante")
+}
+
+else if (experienceScore<= 9000) {
+    console.log ("=ascendente")
+}
+
+else if (experienceScore<=10000) {
+    console.log ("= imortal")
+}
+
+else {
+    console.log ("radiante")
+}
